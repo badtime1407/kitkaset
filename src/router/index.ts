c7e4644ffeea3d569e79f4f994a544ba94eea11d
badtime1407 <badtime1407@gmail.com>
2026-01-22 16:59:06 +0700

    create login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Resetpassword from '../views/Resetpassword.vue'
 
 const routes = [
-  { path: '/', component: Home }
+  { path: '/', component: Home },
+  { path: '/login', component: Login},
+  { path: '/register', component: Register},
+  { path: '/resetpassword', component: Resetpassword},
+
+
 ]
 
 const router = createRouter({
---
+++ src/views/Resetpassword.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Resetpassword</h1>
+</template>
