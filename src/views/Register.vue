bf36e31d404a9f345ee9c4ac4501243063d8efae
befirstsecond14-hub <befirstsecond14@gmail.com>
2026-01-28 15:32:37 +0700

    rugistrr

---
+++ src/views/Register.vue
@@ -0,0 +1,13 @@
+<script setup>
+// พื้นที่สำหรับเขียน Logic (JavaScript/TypeScript)
+</script>
+
+<template>
+  <div>
+    <h1>หน้าลงทะเบียน (Register)</h1>
+  </div>
+</template>
+
+<style scoped>
+/* พื้นที่สำหรับเขียน CSS */
+</style>
